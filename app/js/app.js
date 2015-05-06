b2934dc6b45d3f113be92923ec8b06ac24c95543
Tom Dyer <tdyer1@gmail.com>
2015-05-06 18:04:51 -0400

    Create playlist and song code.

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,17 @@
-/* global Spotify, $*/
+/* global Spotify, $ */
 'use strict';
 
 $(document).ready(function(){
+  // 1. Log something
   console.log('Time to build a Playlist for Spotify');
+
+  // Get the DOM Element with id of 'spotify-songs'
+  // And pass it to the init method for Spotify.Playlist
+  
+  // 2. Invoke the playlist init method, pass it the DOM element
+  // for the ul for the playlist.
+  var songs = Spotify.PlayList.init($('#spotify-songs'));
+
+  // Invoke the Spotify playlist render method.
+  Spotify.PlayList.render(songs);
 });
---
+++ app/js/song.js
@@ -0,0 +1,15 @@
+// create a namespace
+var Spotify = Spotify || {};
+
+// Constructor function for songs
+Spotify.Song = function(songTitle, songDuration, songPrice, songArtist){
+  this.title = songTitle;
+  this.duration = songDuration;
+  this.price = songPrice;
+  this.artist = songArtist;
+};
+
+// This will render this song to HTML
+Spotify.Song.prototype.render = function($playListElement, id){
+  $playListElement.append('<li id="song-' + id + '">' + this.title + '</li>');
+}
