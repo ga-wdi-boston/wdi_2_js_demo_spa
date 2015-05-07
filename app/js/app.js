ba64c6f2aa67311d82226c691b84fb287981ead9
LPayyapilli <luke.payyapilli@gmail.com>
2015-05-06 20:35:31 -0400

    Updated ToDoList(Todoindex.html, App.js, taskList.js) and Spotify (playlist.js, app.js, songs.js, index.html)

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,12 @@
 /* global Spotify, $*/
 'use strict';
-
+// 1. Log something
 $(document).ready(function(){
   console.log('Time to build a Playlist for Spotify');
+
+// 2. Get the DOM element of 'spotify-songs'
+// and pass it to the init method for Spotify.Playlist
+  var songs = Spotify.PlayList.init($('#spotify-songs'));
+// Invoke the Spotify playlist render method for the array songs
+  Spotify.PlayList.render(songs);
 });
