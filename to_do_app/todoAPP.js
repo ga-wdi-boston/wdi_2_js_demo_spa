c7e6e668f291ede2548e128f5927eb67ba7c5cda
aaronw3373 <aaronw3373@gmail.com>
2015-05-06 23:19:28 -0400

    small changes to formatting still working as before

--- to_do_app/todoAPP.js
+++ to_do_app/todoAPP.js
@@ -1,8 +1,8 @@
-//$(document).ready(function() {
+$(document).ready(function() {
   //1. invoke init >go to to_do_list.js
   var tasks = toDoList.taskList.init($('#tasks-list'));
 
   //6 take tasks array and render it go to to_do_list.js
   toDoList.taskList.render(tasks);
-//});
+});
 
