07dc932ac10a67b4c2d8cad169de90d8f9833587
astoellis <astoellis@gmail.com>
2015-05-06 22:15:52 -0400

    finished changes for 5/6, still need "edit" button

--- app/js/app_todo.js
+++ app/js/app_todo.js
@@ -2,13 +2,19 @@
 'use strict';
 
 $(document).ready(function(){
+
+  // 1. Start off by logging that the document has been loaded and it's Time to do stuff!
   console.log('Time to do stuff');
+
+  // 2. Set temporary variable with DOM element so that we're nice and pretty later
   var todoDomElement = $('#todo');
+  // 3. invoke the TaskList's init function, passing the DOM element as the argument.
   var allTasks = Todo.TaskList.init(todoDomElement);
+  // 8. Invoke Render on the now-stored list of tasks (allTasks).
   Todo.TaskList.render(allTasks);
 
 
-
+// 13 these only happen if there's a click, but they add a new li element with name and description.
   $('#getTask').click(function() {
     var tempValue = new Todo.Task($('#inputInfo').val(), $('#description').val());
     tempValue.render(todoDomElement);
--- app/js/tasklist.js
+++ app/js/tasklist.js
@@ -6,9 +6,11 @@ var Todo = Todo || {};
 Todo.TaskList = {
   tasks: [],
   generateTasks: function(){
-  this.tasks.push(new Todo.Task('Do Homework', 'work from GA'));
-  this.tasks.push(new Todo.Task('Get Groceries', 'Lemonade, Cheese, Wine'));
-  this.tasks.push(new Todo.Task('Movie', 'Go to the video store'));
+    // 6. Push 3 pre-determined tasks.
+     // 6.a. create new instance of Todo.Task passing title and description to the  Task Constructor (task.js)
+  this.tasks.push(new Todo.Task('Do Homework', 'work from GA', 2));
+  this.tasks.push(new Todo.Task('Get Groceries', 'Lemonade, Cheese, Wine', 3));
+  this.tasks.push(new Todo.Task('Movie', 'Go to the video store', 4));
   },
   addTask: function(title, description, priority){
     // debugger;
@@ -18,20 +20,20 @@ Todo.TaskList = {
     });
   },
   init: function(appTasklistElement){
-    // 3. set Todo's javascript element (TasklistElement) to the element passed to the init function (DOM ELEMENT).
+    // 4. set Todo's javascript element (TasklistElement) to the element passed to the init function (DOM ELEMENT).
     this.$TasklistElement = appTasklistElement;
+    // 5. Invoke generateTasks, passing no arguments.
     return this.generateTasks();
+    // 7. Return this to the initial invocation
   },
 
   render: function(tasks){
+    // 9. create a for loop to go through each item in the tasks array.
     var id=0;
     this.tasks.forEach(function(task){
+      //10. Initialize the render function (prototype fo the Task object).
       task.render(this.$TasklistElement);
       id++
     }, this);
-  }
-  // removeTask: function(title){
-  //   var indexVal = this.tasks.indexOf(title);
 
-  // }
-};
+}};
